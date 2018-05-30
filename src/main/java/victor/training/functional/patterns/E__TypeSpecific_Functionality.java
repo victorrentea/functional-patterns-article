258a52de5f16536dbca8b4f70d5b3cbe548a5cf8
Victor Rentea <victorrentea@gmail.com>
2018-05-30 09:15:38 +0300

    #5 abstract methods in enum - you cant forget to implement them, but yet- no more extends

--- src/main/java/victor/training/functional/patterns/E__TypeSpecific_Functionality.java
+++ src/main/java/victor/training/functional/patterns/E__TypeSpecific_Functionality.java
@@ -1,31 +1,38 @@
 package victor.training.functional.patterns;
 
-abstract class Movie {
-	public abstract int computePrice(int days);
-}
-
-class RegularMovie extends Movie {
-	public int computePrice(int days) {
-		return days + 1;
+class Movie {
+	enum Type {
+		REGULAR {
+			public int computePrice(int days) {
+				return days + 1;
+			}
+		}, NEW_RELEASE {
+			public int computePrice(int days) {
+				return days * 2;
+			}
+		}, CHILDREN {
+			public int computePrice(int days) {
+				return 5;
+			}
+		};
+		public abstract int computePrice(int days);
 	}
-}
 
-class NewReleaseMovie extends Movie {
-	public int computePrice(int days) {
-		return days * 2;
+	private final Type type;
+
+	public Movie(Type type) {
+		this.type = type;
 	}
-}
 
-class ChildrenMovie extends Movie {
 	public int computePrice(int days) {
-		return 5;
+		return type.computePrice(days);
 	}
 }
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new RegularMovie().computePrice(2));
-		System.out.println(new NewReleaseMovie().computePrice(2));
-		System.out.println(new ChildrenMovie().computePrice(2));
+		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
+		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
+		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
 	}
 }
