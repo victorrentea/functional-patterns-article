cddd01fd19e778ee61262385e7116b9ecf8176e5
Victor Rentea <victorrentea@gmail.com>
2018-05-30 07:12:46 +0300

    #2 Extracted another method to return me a .collected() Map<>; added Product::isNotDeleted

--- src/main/java/victor/training/functional/patterns/B__Stream_Wrecks.java
+++ src/main/java/victor/training/functional/patterns/B__Stream_Wrecks.java
@@ -6,6 +6,7 @@ import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import lombok.Data;
@@ -18,17 +19,19 @@ class ProductService {
 	private ProductRepo productRepo;
 
 	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
+		return getProductCountsOverTheLastYear(orders).entrySet().stream()
+				.filter(e -> e.getValue() >= 10)
+				.map(Entry::getKey)
+				.filter(Product::isNotDeleted)
+				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
+				.collect(toList());
+	}
+
+	private Map<Product, Integer> getProductCountsOverTheLastYear(List<Order> orders) {
 		return orders.stream()
 				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
 				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p -> !p.isDeleted())
-				.filter(p -> !productRepo.getHiddenProductIds().contains(p.getId()))
-				.collect(toList());
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
 	}
 }
 
@@ -41,6 +44,10 @@ class ProductService {
 class Product {
 	private Long id;
 	private boolean deleted;
+	
+	public boolean isNotDeleted() {
+		return !deleted;
+	}
 }
 
 @Data
