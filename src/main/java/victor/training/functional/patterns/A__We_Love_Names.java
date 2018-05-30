f5e43847d03d28a674677d02d5ddff0b58fceb44
Victor Rentea <victorrentea@gmail.com>
2018-05-30 07:10:28 +0300

    #1 externalizing conversion logic in separate class, that. Can now use Dependency Injection.

--- src/main/java/victor/training/functional/patterns/A__We_Love_Names.java
+++ src/main/java/victor/training/functional/patterns/A__We_Love_Names.java
@@ -3,6 +3,7 @@ package victor.training.functional.patterns;
 import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -13,11 +14,37 @@ class UserFacade {
 	
 	private UserRepo userRepo;
 	
+	// @Autowired/@Inject
+	private UserMapper userMapper;
+	
 	public List<UserDto> getAllUsers() {
-		return userRepo.findAll().stream().map(UserDto::new).collect(toList());
+		return userRepo.findAll().stream().map(userMapper::toDto).collect(toList());
 	}
 
 }
+
+// @Component
+class UserMapper {
+	
+	// @Autowired
+	private OtherClass otherClass;
+	
+	public UserDto toDto(User user) {
+		UserDto dto = new UserDto();
+		dto.setUsername(user.getUsername());
+		dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
+		dto.setActive(user.getDeactivationDate() == null);
+		// Code calling a member method of OtherClass
+		return dto;
+	}
+}
+
+// @Component
+class OtherClass {
+	// stuff
+}
+
+
 
 // VVVVVVVVV ==== supporting (dummy) code ==== VVVVVVVVV
 interface UserRepo {
@@ -37,10 +64,4 @@ class UserDto {
 	private String fullName;
 	private String username;
 	private boolean active;
-	
-	public UserDto(User user) {
-		username = user.getUsername();
-		fullName = user.getFirstName() + " " + user.getLastName().toUpperCase();
-		active = user.getDeactivationDate() == null;
-	}
 }
