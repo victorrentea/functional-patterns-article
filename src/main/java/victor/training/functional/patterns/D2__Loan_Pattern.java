31785391dab974910c9148ff24b75be1f750ead5
Victor Rentea <victorrentea@gmail.com>
2018-05-30 08:54:14 +0300

    #4 !WRONG! @Overriding (aka "Hacking") concrete methods in subclasses to change behaviour

--- src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
+++ src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
@@ -2,6 +2,7 @@ package victor.training.functional.patterns;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.stream.Stream;
 
@@ -18,30 +19,12 @@ interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 @Slf4j
 class OrderExporter {
 	
-	public enum ExportType {
-		ORDER, USER
-	}
-	
 	private OrderRepo orderRepo;
 	
-	private UserRepo userRepo; 
-			
-	public File exportFile(String fileName, ExportType exportType) throws Exception {
+	public File exportFile(String fileName) throws Exception {
 		File file = new File("export/" + fileName);
 		try (Writer writer = new FileWriter(file)) {
-			if (exportType == ExportType.ORDER) {
-				writer.write("OrderID;Date\n");
-				orderRepo.findByActiveTrue()
-					.map(o -> o.getId() + ";" + o.getCreationDate())
-					.forEach(Unchecked.consumer(writer::write));
-			} if (exportType == ExportType.USER) {
-				writer.write("Username;FirstName;LastName\n");
-				userRepo.findAll().stream()
-					.map(u -> u.getUsername() + ";" + u.getFirstName() + ";" + u.getLastName())
-					.forEach(Unchecked.consumer(writer::write));
-			} else {
-				throw new IllegalArgumentException();
-			}
+			writeContent(writer);
 			return file;
 		} catch (Exception e) {
 			// TODO send email notification
@@ -49,5 +32,23 @@ class OrderExporter {
 			throw e;
 		}
 	}
+
+	protected void writeContent(Writer writer) throws IOException {
+		writer.write("OrderID;Date\n");
+		orderRepo.findByActiveTrue()
+			.map(o -> o.getId() + ";" + o.getCreationDate())
+			.forEach(Unchecked.consumer(writer::write));
+	}
 }
 
+class UserExporter extends OrderExporter {
+
+	private UserRepo userRepo; 
+	
+	protected void writeContent(Writer writer) throws IOException {
+		writer.write("Username;FirstName;LastName\n");
+		userRepo.findAll().stream()
+			.map(u -> u.getUsername() + ";" + u.getFirstName() + ";" + u.getLastName())
+			.forEach(Unchecked.consumer(writer::write));
+	}
+}
