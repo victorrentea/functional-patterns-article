2d5ac2904374fab0625f91691ccf288c681893ff
Victor Rentea <victorrentea@gmail.com>
2018-05-30 09:14:01 +0300

    #5 OOP-style. abstract methods that have to be overrriden in subclasses.

--- src/main/java/victor/training/functional/patterns/E__TypeSpecific_Functionality.java
+++ src/main/java/victor/training/functional/patterns/E__TypeSpecific_Functionality.java
@@ -1,33 +1,31 @@
 package victor.training.functional.patterns;
 
-class Movie {
-	enum Type {
-		REGULAR, NEW_RELEASE, CHILDREN
-	}
+abstract class Movie {
+	public abstract int computePrice(int days);
+}
 
-	private final Type type;
-
-	public Movie(Type type) {
-		this.type = type;
-	}
-
+class RegularMovie extends Movie {
 	public int computePrice(int days) {
-		switch (type) {
-		case REGULAR:
-			return days + 1;
-		case NEW_RELEASE:
-			return days * 2;
-		case CHILDREN:
-			return 5;
-		default: throw new IllegalArgumentException();
-		}
+		return days + 1;
+	}
+}
+
+class NewReleaseMovie extends Movie {
+	public int computePrice(int days) {
+		return days * 2;
+	}
+}
+
+class ChildrenMovie extends Movie {
+	public int computePrice(int days) {
+		return 5;
 	}
 }
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
-		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
-		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+		System.out.println(new RegularMovie().computePrice(2));
+		System.out.println(new NewReleaseMovie().computePrice(2));
+		System.out.println(new ChildrenMovie().computePrice(2));
 	}
 }
