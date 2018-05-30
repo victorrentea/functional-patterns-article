1d15e24a05d39ef41e5a539a3bfcb7871c7ad23e
Victor Rentea <victorrentea@gmail.com>
2018-05-30 08:50:05 +0300

    #4 !WRONG! Using a boolean to control which export happens- piling everything together

--- src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
+++ src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
@@ -2,13 +2,10 @@ package victor.training.functional.patterns;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Writer;
 import java.util.stream.Stream;
 
 import org.jooq.lambda.Unchecked;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,15 +18,24 @@ interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 @Slf4j
 class OrderExporter {
 	
-	private OrderRepo repo;
+	private OrderRepo orderRepo;
+	
+	private UserRepo userRepo; 
 			
-	public File exportFile(String fileName) throws Exception {
+	public File exportFile(String fileName, boolean isOrderExport) throws Exception {
 		File file = new File("export/" + fileName);
 		try (Writer writer = new FileWriter(file)) {
-			writer.write("OrderID;Date\n");
-			repo.findByActiveTrue()
-				.map(o -> o.getId() + ";" + o.getCreationDate())
-				.forEach(Unchecked.consumer(writer::write));
+			if (isOrderExport) {
+				writer.write("OrderID;Date\n");
+				orderRepo.findByActiveTrue()
+					.map(o -> o.getId() + ";" + o.getCreationDate())
+					.forEach(Unchecked.consumer(writer::write));
+			} else {
+				writer.write("Username;FirstName;LastName\n");
+				userRepo.findAll().stream()
+					.map(u -> u.getUsername() + ";" + u.getFirstName() + ";" + u.getLastName())
+					.forEach(Unchecked.consumer(writer::write));
+			}
 			return file;
 		} catch (Exception e) {
 			// TODO send email notification
