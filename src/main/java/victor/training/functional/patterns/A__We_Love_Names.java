dac027b211b0ca881f8b917b551bdc89324c86ec
Victor Rentea <victorrentea@gmail.com>
2018-05-30 07:02:52 +0300

    #1 Moved conversion to Dto constructor, that I reference using ::new

--- src/main/java/victor/training/functional/patterns/A__We_Love_Names.java
+++ src/main/java/victor/training/functional/patterns/A__We_Love_Names.java
@@ -3,7 +3,6 @@ package victor.training.functional.patterns;
 import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -15,26 +14,10 @@ class UserFacade {
 	private UserRepo userRepo;
 	
 	public List<UserDto> getAllUsers() {
-		return userRepo.findAll().stream().map(this::toDto).collect(toList());
+		return userRepo.findAll().stream().map(UserDto::new).collect(toList());
 	}
 
-	private UserDto toDto(User user) {
-		UserDto dto = new UserDto();
-		dto.setUsername(user.getUsername());
-		dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
-		dto.setActive(user.getDeactivationDate() == null);
-		return dto;
-	}
 }
-
-
-
-
-
-
-
-
-
 
 // VVVVVVVVV ==== supporting (dummy) code ==== VVVVVVVVV
 interface UserRepo {
@@ -54,4 +37,10 @@ class UserDto {
 	private String fullName;
 	private String username;
 	private boolean active;
+	
+	public UserDto(User user) {
+		username = user.getUsername();
+		fullName = user.getFirstName() + " " + user.getLastName().toUpperCase();
+		active = user.getDeactivationDate() == null;
+	}
 }
