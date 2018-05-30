d05e97ab7049aa628e0ca6eab95b116262cbf9f0
Victor Rentea <victorrentea@gmail.com>
2018-05-30 08:51:23 +0300

    #4 !WRONG! using an enum to control the logic that is ran. Still wrong!

--- src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
+++ src/main/java/victor/training/functional/patterns/D2__Loan_Pattern.java
@@ -18,23 +18,29 @@ interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 @Slf4j
 class OrderExporter {
 	
+	public enum ExportType {
+		ORDER, USER
+	}
+	
 	private OrderRepo orderRepo;
 	
 	private UserRepo userRepo; 
 			
-	public File exportFile(String fileName, boolean isOrderExport) throws Exception {
+	public File exportFile(String fileName, ExportType exportType) throws Exception {
 		File file = new File("export/" + fileName);
 		try (Writer writer = new FileWriter(file)) {
-			if (isOrderExport) {
+			if (exportType == ExportType.ORDER) {
 				writer.write("OrderID;Date\n");
 				orderRepo.findByActiveTrue()
 					.map(o -> o.getId() + ";" + o.getCreationDate())
 					.forEach(Unchecked.consumer(writer::write));
-			} else {
+			} if (exportType == ExportType.USER) {
 				writer.write("Username;FirstName;LastName\n");
 				userRepo.findAll().stream()
 					.map(u -> u.getUsername() + ";" + u.getFirstName() + ";" + u.getLastName())
 					.forEach(Unchecked.consumer(writer::write));
+			} else {
+				throw new IllegalArgumentException();
 			}
 			return file;
 		} catch (Exception e) {
